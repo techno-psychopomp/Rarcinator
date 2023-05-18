9dda854039fb40e6fca9f73296ec534d11489119
Brenda Dillon <bdillon@optoro.com>
2023-05-18 19:08:40 -0300

    Commit changes:

--- generate_mr.sh
+++ generate_mr.sh
@@ -18,21 +18,21 @@ commit_message="Commit changes: ${commit_message%, }"
 git commit -m "$commit_message"
 
 # Push the changes to the branch
-git push origin $branch
+#git push origin $branch
 
 # Get the current branch's SHA
-sha=$(git rev-parse HEAD)
+#sha=$(git rev-parse HEAD)
 
 # Create a Pull Request
-response=$(curl -X POST -H "Authorization: Bearer your-access-token" \
+#response=$(curl -X POST -H "Authorization: Bearer your-access-token" \
   -d '{"title": "'"$commit_message"'", "head": "'"$branch"'", "base": "master"}' \
   https://api.github.com/repos/your-username/$repository/pulls)
 
 # Extract the pull request number from the response
-pull_request_number=$(echo $response | jq -r '.number')
+#pull_request_number=$(echo $response | jq -r '.number')
 
 # Generate the pull request URL
-pull_request_url="https://github.com/your-username/$repository/pull/$pull_request_number"
+#pull_request_url="https://github.com/your-username/$repository/pull/$pull_request_number"
 
 # Output the pull request URL
 echo "Pull Request created: $pull_request_url"
