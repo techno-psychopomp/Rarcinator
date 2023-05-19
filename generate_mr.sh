cf66f6861bc0fa991b96724d8ec72964fc22bfe9
Brenda Dillon <bdillon@optoro.com>
2023-05-19 08:59:26 -0300

    more changes

--- generate_mr.sh
+++ generate_mr.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Set the repository name, branch name, and commit message
-repository="your-repository"
-branch="your-branch"
-commit_message=""
+repository="Rarcinator"
+branch="grammar_fixes"
+commit_message="grammar and spelling fixes"
 
 # Add changes to the commit
 git add .
@@ -18,21 +18,21 @@ commit_message="Commit changes: ${commit_message%, }"
 git commit -m "$commit_message"
 
 # Push the changes to the branch
-#git push origin $branch
+git push origin $branch
 
 # Get the current branch's SHA
-#sha=$(git rev-parse HEAD)
+sha=$(git rev-parse HEAD)
 
 # Create a Pull Request
-#response=$(curl -X POST -H "Authorization: Bearer your-access-token" \
-#  -d '{"title": "'"$commit_message"'", "head": "'"$branch"'", "base": "master"}' \
-# https://api.github.com/repos/your-username/$repository/pulls)
+response=$(curl -X POST -H "Authorization: Bearer " \
+  -d '{"title": "'"$commit_message"'", "head": "'"$branch"'", "base": "master"}' \
+ https://api.github.com/repos/techno-psychopomp/$repository/pulls)
 
 # Extract the pull request number from the response
-#pull_request_number=$(echo $response | jq -r '.number')
+pull_request_number=$(echo $response | jq -r '.number')
 
 # Generate the pull request URL
-#pull_request_url="https://github.com/your-username/$repository/pull/$pull_request_number"
+pull_request_url="https://github.com/your-username/$repository/pull/$pull_request_number"
 
 # Output the pull request URL
-#echo "Pull Request created: $pull_request_url"
+echo "Pull Request created: $pull_request_url"
